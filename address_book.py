76585cc8c17b398afb59ae3c94b1bdf37907649c
Lilly <lilly.ashbury@gmail.com>
2023-03-03 10:22:37 -0800

    Removing unused/test files

--- address_book.py
+++
@@ -1,9 +0,0 @@
-class Address_book:
-
-    def __init__(self, name, address, phone_number):
-        self.name = name
-        self.address = address
-        self.phone_number = phone_number
-
-    def __repr__(self):
-        return f"{self.name}: {self.phone_number} - {self.address}"
